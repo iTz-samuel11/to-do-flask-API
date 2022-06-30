41561c02a7ba6bd4ba56f1ec021a38952e0bcddb
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-06-30 22:53:59 +0000

    finalizacion de proyecto

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json 
 
 app = Flask(__name__)
 
@@ -12,8 +12,22 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
+    descoded_object = json.loads(request_body)
+    todos.append(descoded_object)
+    json_list = jsonify(todos)
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    return json_list, 200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    global todos
+    print("This is the position to delete: ",position)
+    new_todos = []
+    for index in range(len(todos)):
+        if index != position:
+            new_todos.append(todos[index])
+    todos = new_todos
+    return jsonify(todos), 200
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
